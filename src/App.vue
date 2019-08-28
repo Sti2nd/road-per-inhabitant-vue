6297a3f9c43623fa68cc064e4e2727c0fb282be0
Stian Jørgensrud <stianj@netcompany.com>
2019-08-28 15:50:59 +0200

    Use material autocomplete element (no data)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MunicipalityInput></MunicipalityInput>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MunicipalityInput from "../src/components/MunicipalityInput";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MunicipalityInput
   }
 }
 </script>
---
+++ src/components/MunicipalityInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <md-autocomplete v-model="selectedMunicipality" :md-options="municipalities" :md-open-on-focus="false">
+    <label>Velg kommune</label>
+  </md-autocomplete>
+</template>
+
+<script>
+export default {
+  name: 'MunicipalityInput',
+  props: {
+  },
+  data: () => (
+    {
+      municipalities: ["Oslo", "Bærum", "Trondheim"],
+      selectedMunicipality: null,
+    }
+  )
+}
+</script>
+
+<style scoped>
+
+</style>
