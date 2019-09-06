38da631bfb0bc241a1ac58f37aea7c4fe314ff61
Stian Jørgensrud <stianj@netcompany.com>
2019-09-06 13:21:46 +0200

    remove jest, puppeteer and configs

--- jest-puppeteer.config.js
+++
@@ -1,8 +0,0 @@
-// See https://github.com/smooth-code/jest-puppeteer for more information about these options
-module.exports = {
-  server: {
-    command: 'npm run serve',
-    port: 8080,
-    launchTimeout: 100000,
-  },
-};
